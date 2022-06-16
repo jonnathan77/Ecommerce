6a52fc49ac9065a5f3c580e00b8bb393f3a5649d
jonnathan77 <jonnathanfsouzacruz@gmail.com>
2022-06-15 22:04:55 -0300

    commitando na dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { ModalController } from '@ionic/angular';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -10,8 +11,11 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,ModalController],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+
 })
-export class AppModule {}
+export class AppModule {
+  
+}
